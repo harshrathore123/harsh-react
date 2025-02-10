7fb6989625014c8da3b76bae115635b8e26c09cd
Harsh <rathorehrharsh5@gmail.com>
2025-02-10 10:26:59 +0530

    changes in react router dom

---
+++ 07-ReactRouterDom/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Footer from './Component/Footer/Footer'
+import Header from './Component/Header/Header'
+
+function App() {
+
+  return (
+    <>
+    <Header/>
+    
+    <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ 07-ReactRouterDom/src/Component/GitHub/GitHub.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function GitHub() {
+  return (
+    <>
+    </>
+  )
+}
+
+export default GitHub
